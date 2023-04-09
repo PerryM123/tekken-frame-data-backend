2e406da16f2f63dbf56cf0e76ee1aa57a8597e0e
perry.miranda <perrymiranda1@yahoo.com>
2023-04-10 02:13:40 +0900

    serverLoggerを追加

--- controller/characterList.js
+++ controller/characterList.js
@@ -5,6 +5,17 @@ const BOOLEAN = {
   false: 0
 }
 
+const LOGGER_TYPE = {
+  ERROR: 'error',
+  WARNING: 'warn',
+  INFO: 'info'
+}
+
+// TODO: 定数化する必要ある
+const serverLogger = (type, message) => {
+  console.log(`[${type}] `, message);
+}
+
 export const getCharacterList = (req, res) => {
   database.query("SELECT * from characters", (error, rows) => {
     if (!error) {
@@ -15,10 +26,10 @@ export const getCharacterList = (req, res) => {
         }
       }));
     }
+    serverLogger(LOGGER_TYPE.ERROR, error);
     return res.status(500).json({
       message: "error",
-      // TODO: 分かりやすいエラーコードに変更
-      code: "ERR_GET_ERROR"
+      code: "ERR_GET_CHARACTER_ERROR"
     });
   })
 }
@@ -26,6 +37,7 @@ export const getCharacterList = (req, res) => {
 export const getSpecificCharacter = (req, res) => {
   const name = req.params.name;
   if (!req.params || req.params.name === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'ERR_MISSING_GET_SPECIFIC_CHARACTER');
     return res.status(400).json({
       message: "missing query (name)",
       code: "ERR_MISSING_GET_SPECIFIC_CHARACTER"
@@ -44,6 +56,7 @@ export const getSpecificCharacter = (req, res) => {
         is_completed: rows[0].is_completed === BOOLEAN.true
       });
     }
+    serverLogger(LOGGER_TYPE.ERROR, error);
     return res.status(500).json({
       message: "error",
       code: "ERR_GET_SPECIFIC_CHARACTER_ERROR"
@@ -54,12 +67,14 @@ export const getSpecificCharacter = (req, res) => {
 export const addCharacter = (req, res) => {
   const body = req.body;
   if (body.name === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'パラメータが足りない');
     return res.status(400).json({
       message: "missing body (name)",
       code: "ERR_MISSING_BODY_POST"
     });
   }
   if (body.is_completed === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'パラメータが足りない');
     return res.status(400).json({
       message: "missing body (is_completed)",
       code: "ERR_MISSING_BODY_POST"
@@ -67,9 +82,10 @@ export const addCharacter = (req, res) => {
   }
   database.query("select * from characters where name=?;", [body.name], (error, rows) => {
     if (error) {
+      serverLogger(LOGGER_TYPE.ERROR, error);
       return res.status(500).json({
-        // TODO: loggerにerror内容を出力し、以下のメッセージに別のエラーコードに変更
-        message: `${error}`
+        message: "error",
+        code: "ERR_ADD_CHARACTER_ERROR"
       });
     }
     if (!rows.length) {
@@ -79,12 +95,14 @@ export const addCharacter = (req, res) => {
             message: "POST success"
           });
         }
+        serverLogger(LOGGER_TYPE.ERROR, error);
         return res.status(500).json({
-          // TODO: loggerにerror内容を出力し、以下のメッセージに別のエラーコードに変更
-          message: `${error}`
+          message: "error",
+          code: "ERR_ADD_CHARACTER_ERROR"
         });
       })
     } else {
+      serverLogger(LOGGER_TYPE.WARNING, 'すでに登録');
       return res.status(409).json({
         message: "キャラクターはすでに登録されてる",
         code: "ERR_ALREADY_ADDED"
@@ -96,6 +114,7 @@ export const addCharacter = (req, res) => {
 export const deleteCharacter = (req, res) => {
   const body = req.body;
   if (body.name === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'パラメータが足りない');
     return res.status(400).json({
       message: "missing body (name)",
       code: "ERR_MISSING_BODY_DELETE"
@@ -103,9 +122,10 @@ export const deleteCharacter = (req, res) => {
   }
   database.query("select * from characters where name=?;", [body.name], (error, rows) => {
     if (error) {
+      serverLogger(LOGGER_TYPE.ERROR, error);
       return res.status(500).json({
-        // TODO: loggerにerror内容を出力し、以下のメッセージに別のエラーコードに変更
-        message: `${error}`
+        message: "error",
+        code: "ERR_DELETE_CHARACTER_ERROR"
       });
     }
     if (rows.length) {
@@ -115,12 +135,14 @@ export const deleteCharacter = (req, res) => {
             message: "DELETE success"
           });
         }
+        serverLogger(LOGGER_TYPE.ERROR, error);
         return res.status(500).json({
-          // TODO: loggerにerror内容を出力し、以下のメッセージに別のエラーコードに変更
-          message: `${error}`
+          message: "error",
+          code: "ERR_DELETE_CHARACTER_ERROR"
         });
       })
     } else {
+      serverLogger(LOGGER_TYPE.ERROR, '存在しない');
       return res.status(404).json({
         message: "キャラクターは存在されてない",
         code: "ERR_NOT_FOUND"
@@ -133,12 +155,14 @@ export const updateCharacterName = (req, res) => {
   const body = req.body;
   const name = req.params.name;
   if (body.name === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'パラメータ足りない');
     return res.status(400).json({
       message: "missing body (name)",
       code: "ERR_MISSING_BODY_PUT"
     });
   }
   if (!req.params || req.params.name === undefined) {
+    serverLogger(LOGGER_TYPE.WARNING, 'パラメータ足りない');
     return res.status(400).json({
       message: "missing params (name)",
       code: "ERR_MISSING_PARAMS_PUT"
@@ -150,9 +174,10 @@ export const updateCharacterName = (req, res) => {
         message: "PUT success"
       });
     }
+    serverLogger(LOGGER_TYPE.ERROR, error);
     return res.status(500).json({
-      // TODO: loggerにerror内容を出力し、以下のメッセージに別のエラーコードに変更
-      message: `${error}`
+      message: "error",
+      code: "ERR_UPDATE_CHARACTER_ERROR"
     });
   })
 }
