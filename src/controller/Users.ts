97e6b5e638b5cf45a1c0c70f6b178cd271a2c400
perry.miranda <perrymiranda1@yahoo.com>
2023-07-26 17:33:51 +0900

    loginができたらユーザIDも提供するように修正

--- src/controller/Users.ts
+++ src/controller/Users.ts
@@ -46,7 +46,8 @@ export const authenticateUser = async (req, res) => {
   if (convertToMd5(password) === userData.password) {
     req.session.isAuthenticatedUser = true;
     res.status(200).json({
-      isSuccess: true
+      isSuccess: true,
+      userId: userData.id
     });
   } else {
     res.status(401).json({
